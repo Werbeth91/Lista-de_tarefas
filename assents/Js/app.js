d15a4170ef308ac3704f348388538cd65b3abadc
werbeth <werbeth1991pbs@gmail.com>
2022-07-12 15:09:52 -0300

    refactor: melhorias no arquivo javaScript

--- assents/Js/app.js
+++ assents/Js/app.js
@@ -1,65 +1,76 @@
-const inputNovaTarefa = document.querySelector(".input-nova-tarefa");
-const btnAddTarefa = document.querySelector(".add-tarefa");
-const novasTarefas = document.querySelector(".novas-tarefas");
+const inputNewTask = document.querySelector(".input-new-task");
+const btnNewTask = document.querySelector(".add-new-task");
+const tasksList = document.querySelector(".tasks-list");
 
-function criandoNovaTarefa(inputText) {
-  const li = document.createElement("li");
-  li.innerHTML = inputText;
-  novasTarefas.appendChild(li);
-  criaBotaoApagar(li);
-  salvandoTarefas();
-}
-
-function criaBotaoApagar(li) {
-  li.innerText += " ";
-  const botaoApagarTarefa = document.createElement("button");
-  botaoApagarTarefa.innerText = "Apagar";
-  botaoApagarTarefa.setAttribute("class", "apagar");
-  li.appendChild(botaoApagarTarefa);
-}
-
-function salvandoTarefas() {
-  const tarefasLi = novasTarefas.querySelectorAll("li");
-  const listaDasTarefas = [];
-
-  for (let tarefas of tarefasLi) {
-    let textoDaTarefa = tarefas.innerText;
-    textoDaTarefa = textoDaTarefa.replace("Apagar", "").trim();
-    listaDasTarefas.push(textoDaTarefa);
+inputNewTask.addEventListener("keypress", (e) => {
+  if (e.keyCode == 13) {
+    let task = {
+      nome: inputNewTask.value,
+      id: generateId(),
+    };
+    addTask(task);
   }
-
-  const tarefasJson = JSON.stringify(listaDasTarefas);
-  localStorage.setItem("tarefas", tarefasJson);
-}
-function recarregandoTarefaSalvas() {
-  const tarefas = localStorage.getItem("tarefas");
-  const listaDasTarefas = JSON.parse(tarefas);
-
-  for (let tarefa of listaDasTarefas) {
-    criandoNovaTarefa(tarefa);
-  }
-}
-recarregandoTarefaSalvas();
-
-btnAddTarefa.addEventListener("click", function () {
-  if (!inputNovaTarefa.value) return;
-  criandoNovaTarefa(inputNovaTarefa.value);
-  inputNovaTarefa.value = "";
-  inputNovaTarefa.focus();
 });
 
-inputNovaTarefa.addEventListener("keypress", function (event) {
-  if (event.keyCode === 13) {
-    criandoNovaTarefa(inputNovaTarefa.value);
-    inputNovaTarefa.value = "";
-    inputNovaTarefa.focus();
-  }
-  document.addEventListener("click", function (event) {
-    const element = event.target;
+btnNewTask.addEventListener("click", (e) => {
+  let task = {
+    nome: inputNewTask.value,
+    id: generateId(),
+  };
+  addTask(task);
+});
 
-    if (element.classList.contains("apagar")) {
-      element.parentElement.remove();
-      salvandoTarefas();
+function generateId() {
+  return Math.floor(Math.random() * 1000);
+}
+
+function addTask(task) {
+  let li = createTagLi(task);
+  tasksList.appendChild(li);
+  inputNewTask.value = "";
+}
+
+function createTagLi(task) {
+  let li = document.createElement("li");
+  li.id = task.id;
+
+  let span = document.createElement("span");
+  span.classList.add("text-task");
+  span.innerHTML = task.nome;
+
+  let div = document.createElement("div");
+
+  let btnEdit = document.createElement("button");
+  btnEdit.classList.add("icons-tasks-list");
+  btnEdit.innerHTML = '<i class="fa-solid fa-pencil"></i>';
+  btnEdit.setAttribute("onclick", "editTask(" + task.id + ")");
+
+  let btnDelete = document.createElement("button");
+  btnDelete.classList.add("icons-tasks-list");
+  btnDelete.innerHTML = '<i class="fa-solid fa-trash"></i>';
+  btnDelete.setAttribute("onclick", "deleteTask(" + task.id + ")");
+
+  div.appendChild(btnEdit);
+  div.appendChild(btnDelete);
+
+  li.appendChild(span);
+  li.appendChild(div);
+  return li;
+}
+
+function editTask(idTask) {
+  let li = document.getElementById("" + idTask + "");
+  if (li) {
+    li.firstChild.classList.toggle("completed");
+  }
+}
+
+function deleteTask(idTask) {
+  let confirmation = window.confirm("Realmente deseja excluir essa tarefa?");
+  if (confirmation) {
+    let li = document.getElementById("" + idTask + "");
+    if (li) {
+      tasksList.removeChild(li);
     }
-  });
-});
+  }
+}
